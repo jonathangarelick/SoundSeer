6c0ea110f7e76eb81bed0594d572330d722e64d2
jonathangarelick <41495718+jonathangarelick@users.noreply.github.com>
2024-04-12 07:37:22 -0700

    Initial commit

---
+++ SoundSeer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SoundSeer
+//
+//  Created by Jonathan Garelick on 4/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SoundSeer/SoundSeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundSeerApp.swift
+//  SoundSeer
+//
+//  Created by Jonathan Garelick on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SoundSeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
