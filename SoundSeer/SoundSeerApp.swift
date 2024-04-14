0c7101f83989757b1f5303ee05a97060c4771d34
jonathangarelick <41495718+jonathangarelick@users.noreply.github.com>
2024-04-14 13:24:49 -0700

    Move link dump to #7

--- SoundSeer/SoundSeerApp.swift
+++ SoundSeer/SoundSeerApp.swift
@@ -63,38 +63,3 @@ struct SoundSeerApp: App {
 
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// https://stackoverflow.com/questions/60707286/how-to-add-click-event-to-whole-view-in-swiftui
-
-
-// text stop display at "Everyday (feat. Rod Stewart, Miguel & Mark Ronso" (that's 48 chars)
-// https://sarunw.com/posts/swiftui-menu-bar-app
-// https://blog.schurigeln.com/menu-bar-apps-swift-ui/
-// tccutil reset AppleEvents net.garelick.SoundSeer
-// https://joshspicer.com/applescript
-// https://stackoverflow.com/questions/77131963/how-to-identify-if-the-menubarextra-in-macos-is-hidden-by-the-system
-// https://github.com/kmikiy/SpotMenu/issues/141
-// Concurrency: https://developer.apple.com/library/archive/documentation/General/Conceptual/ConcurrencyProgrammingGuide/OperationQueues/OperationQueues.html#//apple_ref/doc/uid/TP40008091-CH102-SW1
-
