cc13776830d089b85c965d2c41a7f18e4073e06b
jonathangarelick <41495718+jonathangarelick@users.noreply.github.com>
2024-05-03 09:38:15 -0700

    Handle crash when Spotify is not installed

--- SoundSeer/SoundSeerApp.swift
+++ SoundSeer/SoundSeerApp.swift
@@ -4,25 +4,27 @@ import SwiftUI
 
 @main
 struct SoundSeerApp: App {
-    @StateObject private var spotifyViewModel: SpotifyViewModel = SpotifyViewModel()
-    
     @State private var isOpenAtLoginEnabled: Bool = SMAppService.mainApp.status == .enabled
-    @State private var window: NSWindow?
-    
-    var body: some Scene {
-        MenuBarExtra {
+
+    private var spotifyViewModel: SpotifyViewModel? = SpotifyViewModel()
+
+    // https://stackoverflow.com/a/67065308
+    private struct SongMetaContent: View {
+        @StateObject var spotifyViewModel: SpotifyViewModel
+
+        var body: some View {
             if spotifyViewModel.playerState == .playing, spotifyViewModel.prefixLength <= 0 {
                 Button("Not enough room for song. Try restarting.", systemImage: "exclamationmark.triangle", action: {})
                     .labelStyle(.titleAndIcon)
                     .disabled(true)
             }
-            
+
             Button("Next Track", systemImage: "forward.end", action: spotifyViewModel.nextTrack)
                 .labelStyle(.titleAndIcon)
                 .disabled(spotifyViewModel.playerState != .paused && spotifyViewModel.playerState != .playing)
-            
+
             Divider()
-            
+
             Button(!spotifyViewModel.currentSong.isEmpty
                    ? (spotifyViewModel.prefixLength > 0
                       ? spotifyViewModel.currentSong.truncate(length: Int(Double(spotifyViewModel.prefixLength) * 1.5))
@@ -30,7 +32,7 @@ struct SoundSeerApp: App {
                    : "Song unknown", systemImage: "music.note", action: spotifyViewModel.openCurrentSong)
             .labelStyle(.titleAndIcon)
             .disabled(spotifyViewModel.currentSongId.isEmpty)
-            
+
             Button(!spotifyViewModel.currentArtist.isEmpty
                    ? (spotifyViewModel.prefixLength > 0
                       ? spotifyViewModel.currentArtist.truncate(length: Int(Double(spotifyViewModel.prefixLength) * 1.5))
@@ -38,7 +40,7 @@ struct SoundSeerApp: App {
                    : "Artist unknown", systemImage: "person", action: spotifyViewModel.openCurrentArtist)
             .labelStyle(.titleAndIcon)
             .disabled(spotifyViewModel.currentSongId.isEmpty)
-            
+
             Button(!spotifyViewModel.currentAlbum.isEmpty
                    ? (spotifyViewModel.prefixLength > 0
                       ? spotifyViewModel.currentAlbum.truncate(length: Int(Double(spotifyViewModel.prefixLength) * 1.5))
@@ -46,21 +48,55 @@ struct SoundSeerApp: App {
                    : "Album unknown", systemImage: "opticaldisc", action: spotifyViewModel.openCurrentAlbum)
             .labelStyle(.titleAndIcon)
             .disabled(spotifyViewModel.currentSongId.isEmpty)
-            
+
             Divider()
-            
+
             Button("Copy Spotify URL", systemImage: "doc.on.doc", action: spotifyViewModel.copySpotifyExternalURL)
                 .labelStyle(.titleAndIcon)
                 .disabled(spotifyViewModel.currentSongId.isEmpty)
-            
+        }
+    }
+
+    private struct LabelContent: View {
+        @StateObject var spotifyViewModel: SpotifyViewModel
+        @State private var window: NSWindow?
+
+        var body: some View {
+            Group {
+                if spotifyViewModel.playerState != .playing || spotifyViewModel.nowPlaying.isEmpty {
+                    Image(systemName: "ear")
+                } else {
+                    Text(spotifyViewModel.nowPlaying)
+                }
+            }
+            .onReceive(NotificationCenter.default.publisher(for: NSWindow.didChangeOcclusionStateNotification)) { notification in
+                Logger.view.debug("Received didChangeOcclusionState notification")
+
+                guard let window = notification.object as? NSWindow else { return }
+                spotifyViewModel.isAppVisibleInMenuBar = window.occlusionState.contains(.visible)
+            }
+            .background(WindowAccessor(window: $window))
+        }
+    }
+
+    var body: some Scene {
+        MenuBarExtra {
+            if let spotifyViewModel = spotifyViewModel {
+                SongMetaContent(spotifyViewModel: spotifyViewModel)
+            } else {
+                Button("Spotify app not found.", systemImage: "exclamationmark.triangle", action: {})
+                    .labelStyle(.titleAndIcon)
+                    .disabled(true)
+            }
+
             Divider()
-            
+
             Button {
                 // This is a bit of a hack. First, toggle the @State variable to immediately update the UI.
                 // Perform the state modification, then re-update the @State variable with the source of truth
                 do {
                     isOpenAtLoginEnabled.toggle()
-                    
+
                     if SMAppService.mainApp.status == .enabled {
                         try SMAppService.mainApp.unregister()
                     } else {
@@ -76,23 +112,16 @@ struct SoundSeerApp: App {
                 }
                 Text("Open at Login")
             }
-            
-            Button("Quit", action: spotifyViewModel.quitSoundSeer)
+
+            Button("Quit") {
+                NSApplication.shared.terminate(nil)
+            }
         } label: {
-            Group {
-                if spotifyViewModel.playerState != .playing || spotifyViewModel.nowPlaying.isEmpty {
-                    Image(systemName: "ear")
-                } else {
-                    Text(spotifyViewModel.nowPlaying)
-                }
+            if let spotifyViewModel = spotifyViewModel {
+                LabelContent(spotifyViewModel: spotifyViewModel)
+            } else {
+                Image(systemName: "ear")
             }
-            .onReceive(NotificationCenter.default.publisher(for: NSWindow.didChangeOcclusionStateNotification)) { notification in
-                Logger.view.debug("Received didChangeOcclusionState notification")
-                
-                guard let window = notification.object as? NSWindow else { return }
-                spotifyViewModel.isAppVisibleInMenuBar = window.occlusionState.contains(.visible)
-            }
-            .background(WindowAccessor(window: $window))
         }
     }
 }
