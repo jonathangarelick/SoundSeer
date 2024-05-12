3bd7fff74482cc5287a951f0e1e243e52d2ef5d3
jonathangarelick <41495718+jonathangarelick@users.noreply.github.com>
2024-05-11 19:25:09 -0700

    Add empty Secrets.swift to fix build

---
+++ SoundSeer/Secrets.swift
@@ -0,0 +1,12 @@
+enum Secrets {
+    enum Music {
+        static let keyId = ""
+        static let secret = ""
+        static let teamId = ""
+    }
+    
+    enum Spotify {
+        static let clientID: String = ""
+        static let clientSecret: String = ""
+    }
+}
